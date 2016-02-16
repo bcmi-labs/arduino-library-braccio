dbed81dda45c5ccedd54fba03d746bfd5968d42a
astronomer80 <astronomer@tiscali.it>
2016-02-16 10:18:55 +0100

    Update testBraccio90.ino

--- examples/testBraccio90/testBraccio90.ino
+++ examples/testBraccio90/testBraccio90.ino
@@ -1,9 +1,10 @@
+ /*
+  testBraccio90.ino
 
-/*
-  testBraccio90
-
- This sketch testBraccio90 move all servo motors ton 90 degrees
-
+ testBraccio90 is a setup sketch to check the alignment of all the servo motors
+ This is the first sketch you need to run on Braccio
+ When you start this sketch Braccio will be positioned perpendicular to the base
+ If you can't see the Braccio in this exact position you need to reallign the servo motors position
 
  Created on 18 Nov 2015
  by Andrea Martino
@@ -11,6 +12,7 @@
  This example is in the public domain.
  */
 
+#include <Braccio.h>
 #include <Servo.h>
 
 
@@ -21,24 +23,34 @@ Servo wrist_rot;
 Servo wrist_ver;
 Servo tongue;
 
-
 void setup() {
-	//Initialization  the servo motor pins
-	base.attach(11);
-	shoulder.attach(10);
-	elbow.attach(9);
-	wrist_rot.attach(6);
-	wrist_ver.attach(5);
-	tongue.attach(3);
-
+  //Initialization functions for Braccio
+  Braccio.Initialization();
+  
+  //Set up the initial position for Braccio
+  //All the servo motors will be positioned in the "safety" position:
+  //Base (M1):180 degrees
+  //Shoulder (M2): 45 degrees
+  //Elbow (M3): 180 degrees
+  //Wrist vertical (M4): 90 degrees
+  //Wrist rotation (M5): 90 degrees
+  //Tongue (M6): 10 degrees
+  Braccio.InitArm();  
 }
 
 void loop() {
-	//Move all the servo motors to 90 degrees	
-	base.write(90);
-	shoulder.write(90);
-	elbow.write(90);
-	wrist_rot.write(90);
-	wrist_ver.write(90);
-	tongue.write(90);
+  /*
+   Step Delay: a milliseconds delay between the movement of each servo.  Allowed values from 10 to 30 msec.
+   M1=base degrees. Allowed values from 15 to 165 degrees
+   M2=shoulder degrees. Allowed values from 0 to 180 degrees
+   M3=elbow degrees. Allowed values from 0 to 180 degrees
+   M4=wrist vertical degrees. Allowed values from 0 to 170 degrees
+   M5=wrist rotation degrees. Allowed values from 0 to 180 degrees
+   M6=tongue degrees. Allowed values from 10 to 73 degrees. 10: the toungue is open, 73: the tongue is closed.
+  */
+  
+  // the arm is aligned upwards  and the tongue is closed
+                     //(step delay, M1, M2, M3, M4, M5, M6);
+  Braccio.ServoMovement(20,         90, 90, 90, 90, 90,  73);  
+	 
 }
